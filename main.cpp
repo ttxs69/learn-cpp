925f84e7e750671cee19688ac8aaa108e30fca9e
sarace <ttxs69@huisa.win>
2022-08-06 09:27:51 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @author sarace (ttxs69@huisa.win)
+ * @brief
+ * @version 0.1
+ * @date 2022-08-06
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#include <iostream>
+int main() {
+  // print hello world to stdout
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
