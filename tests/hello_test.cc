89da3eef9074ac034d7cdb0c8a8c496fbcabb2eb
sarace <sarace@huisa.win>
2022-10-01 11:04:18 +0800

    add error msg for tests when failed

--- tests/hello_test.cc
+++ tests/hello_test.cc
@@ -11,10 +11,14 @@ TEST(HelloTest, BasicAssertions) {
 
 TEST(BinarySearchTest, BinarySearchWithWhile) {
   std::vector<int> normal = {1, 2, 3, 4, 5};
-  EXPECT_EQ(BinarySearchWithWhile(normal, 2), 1);
+  EXPECT_EQ(BinarySearchWithWhile(normal, 2), 1)
+      << "2 should be found at 1, but output is "
+      << BinarySearchWithWhile(normal, 2);
 }
 
 TEST(BinarySearchTest, BinarySearchWithWhileNotFound) {
   std::vector<int> normal = {1, 2, 3, 4, 5};
-  EXPECT_EQ(BinarySearchWithWhile(normal, 6), -1);
+  EXPECT_EQ(BinarySearchWithWhile(normal, 6), -1)
+      << "6 should not be found, but result is "
+      << BinarySearchWithWhile(normal, 6);
 }
